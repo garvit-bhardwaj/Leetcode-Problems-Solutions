009d23556dd620589bf9f70acb1ddc72e2404774
AdityaV05 <95166684+AdityaV05@users.noreply.github.com>
2022-10-04 16:41:28 +0530

    Create maxgap.cpp

---
+++ maxgap.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+       
+
+    sort(nums.begin(),nums.end());
+    int ans=0;
+    for(int i=1;i<nums.size();i++){
+        ans=max(ans,abs(nums[i]-nums[i-1]));
+    }
+                return ans;
+}
+};
