68600a3d060a2608508f015af50367a23f34f831
Riti Rathore <109359450+ritirathore@users.noreply.github.com>
2022-10-18 00:01:10 +0530

    Create Pangram.cpp

---
+++ Pangram.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<char> st (sentence.begin(), sentence.end());         
+    return st.size()==26;
+    }
+};
