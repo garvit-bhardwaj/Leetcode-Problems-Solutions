3490041bcd4f0109b70901e13616229f0a9060f4
Akruti <aviternal20@gmail.com>
2022-10-19 09:13:37 +0530

    Changes made by akruti, added a new python problem

---
+++ Roman_to_integer.py
@@ -0,0 +1,18 @@
+#Problem link - https://leetcode.com/problems/roman-to-integer/
+
+#Solution
+class Solution:
+ 
+
+    def romanToInt(self, s):
+        roman = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        prev, total = 0, 0
+        for c in s:
+            curr = roman[c]
+            total += curr
+            # need to subtract
+            if curr > prev:
+                total -= 2 * prev
+            prev = curr
+        return total
