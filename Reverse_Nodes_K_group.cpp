d3d3b2433690b54e73cacbc01b1b42277a4b522f
Sayahnneeta Dutta <sayahnneetadutta@gmail.com>
2022-10-27 02:45:02 +0530

    Reverse Nodes in K group cpp code added

--- Reverse_Nodes_K_group.cpp
+++ Reverse_Nodes_K_group.cpp
@@ -1,9 +1,3 @@
-//Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
-
-//k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
-
-//You may not alter the values in the list's nodes, only nodes themselves may be changed
-
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
